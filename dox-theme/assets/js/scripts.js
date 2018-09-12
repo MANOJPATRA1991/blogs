bf7f763ab27f02f0dea5df20269e35a7e5f1652f
Alen Vuletić <alenvuletic@gmail.com>
2018-09-12 14:37:23 +0200

    T466: Added responsive table objects to post component.

--- dox-theme/assets/js/scripts.js
+++ dox-theme/assets/js/scripts.js
@@ -2,17 +2,24 @@
 	'use strict';
 
 	/**
-	 * Hamburger icon toggle.
+	 * Hamburger icon toggle
 	 */
 	$( '.js-hamburger-icon' ).on( 'click', function() {
 		$( '.js-hamburger-icon' ).toggleClass( 'is-active' );
 	} );
 
 	/**
-	 * Offcanvas toggle.
+	 * Offcanvas toggle
 	 */
 	$( '.js-offcanvas-toggle' ).on( 'click', function() {
 		$( '.c-offcanvas' ).toggleClass( 'is-active' );
 		$( '.js-logo' ).toggleClass( 'u-text-gray-300' );
+	} );
+
+	/**
+	 * Post content manipulations
+	 */
+	$( document ).ready( function() {
+		$( '.js-post' ).find( 'table' ).wrap( '<div class="o-responsive-table"></div>' );
 	} );
 }( jQuery ) );
